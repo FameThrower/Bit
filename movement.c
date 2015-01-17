1e426b1f15250ef296371c1a7bc002b92b84fc34
ryanbrinkley <ryanbrinkley@yahoo.com>
2015-01-17 10:44:26 -0600

    fixed backtracking (maybe)
    idk its confusing

--- movement.c
+++ movement.c
@@ -20,8 +20,13 @@
 
 struct ryans{
 	int noWall[4]; //north = 0, east = 1, south = 2, west = 3
-	int prev;
+	
+	int prevRow; //previous row , column, direction. used for backtracking
+	int prevCol;
+	int prevDir;
+	
 	char opening;
+	char newSquare; //first time entering new square, so use sensors to determine wall placement
 };
 
 int facing = NORTH; //robot starts the maze facing NORTH
@@ -35,52 +40,91 @@ void init(){
 			ryan[i][x].noWall[SOUTH] = 0;
 			ryan[i][x].noWall[EAST] = 0;
 			ryan[i][x].noWall[WEST] = 0;
-			ryan[i][x].prev = 0;
+			ryan[i][x].prevRow = -1;
+			ryan[i][x].prevCol = -1;
+			ryan[i][x].prevDir = -1;
 			ryan[i][x].opening = FOUR_DIR;
+			ryan[i][x].newSquare = 1;
 		}
 	}
 }
 
 void discover(int dir, int row, int col){
 	ryan[row][col].opening >> 1;
-	if(dir){
-		ryan[row][col].prev = dir;
+	if(ryan[row][col].newSquare == 1){ //robot has not been to his square before, so check for walls
+		isWall(row,col);
+		ryan[row][col].newSquare == 0;
 	}
-	isWall(row,col);
-	if(ryan[row][col].noWall[EAST] && 
+	
+	//moving east
+	if(ryan[row][col].noWall[EAST] &&  
 	ryan[row][col].opening && 
 	!(dir & WEST)) {
 	        ryan[row][col].opening >> 1;
 		move(EAST);
+		
+		if(ryan[row+1][col].prevRow < 0){ //keep track of previous row/col for backtracking
+			ryan[row+1][col].prevRow = row;
+			ryan[row+1][col].prevCol = col;
+			ryan[row][col-1].prevDir = WEST;
+		}
 		discover(WEST, ++row,col);
 	}
+	
+	//moving north
 	if(ryan[row][col].noWall[NORTH] && 
 	ryan[row][col].opening && 
 	!(dir & SOUTH)) {
 		ryan[row][col].opening >> 1;
 		move(NORTH);
+		
+		if(ryan[row][col+1].prevRow < 0){ //keep track of previous row/col for backtracking
+			ryan[row][col+1].prevRow = row;
+			ryan[row][col+1].prevCol = col;
+			ryan[row][col-1].prevDir = SOUTH;
+		}
 		discover(SOUTH, row,++col);
 	}
+	
+	//moving west
 	if(ryan[row][col].noWall[WEST] && 
 	ryan[row][col].opening && 
 	!(dir & EAST)) {
 		ryan[row][col].opening >> 1;
 		move(WEST);
+		
+		if(ryan[row-1][col].prevRow < 0){ //keep track of previous row/col for backtracking
+			ryan[row-1][col].prevRow = row;
+			ryan[row-1][col].prevCol = col;
+			ryan[row][col-1].prevDir = EAST;
+		}
 		discover(EAST, --row,col);	
 	}
+	
+	//moving south
 	if(ryan[row][col].noWall[SOUTH] && 
 	ryan[row][col].opening && 
 	!(dir & NORTH)) {
 		ryan[row][col].opening >> 1;
 		move(SOUTH);
+		
+		if(ryan[row][col-1].prevRow < 0){ //keep track of previous row/col for backtracking
+			ryan[row][col-1].prevRow = row;
+			ryan[row][col-1].prevCol = col;
+			ryan[row][col-1].prevDir = NORTH;
+		}
 		discover(NORTH, row,--col);
 	}
-	if(!ryan[row][col].opening) MOVE(dir); //reverse
+	
+	//backtracking if there are no more squares to explore from the current square
+	if(!ryan[row][col].opening){
+		move(ryan[row][col].prevDir);
+		discover((ryan[row][col].prevDir + 2) % 4,ryan[row][col].prevRow,ryan[row][col].prevCol);
+	} 
 	
 }
 
 //keep track of direction robot is facing
-//sends command to PSOC
 //faces the correct direction, then moves forward 12 inches
 //returns distance moved
 //
@@ -103,7 +147,7 @@ int move(int direction){
 	}
 	moveForward();//advance to a new square, which is 12 inches ahead
 	
-	//should return 
+	//should return distance moved
 	return 0;
 }
 
@@ -145,18 +189,18 @@ void isWall(int row, int col){
 	}
 	
 	i = getDistLeft((facing-1) % 4);
-    if(i > WALL) {
+    	if(i > WALL) {
 		ryan[row][col].noWall[(facing-1) % 4] = 1;
 	}else{
-        ryan[row][col].opening >> 1;
-    }
+        	ryan[row][col].opening >> 1;
+    	}
 	
 	i = getDistRight((facing+1) % 4);
-    if(i > WALL) {
+    	if(i > WALL) {
 		ryan[row][col].noWall[(facing+1) % 4] = 1;
 	}else{
-        ryan[row][col].opening >> 1;
-    }
+        	ryan[row][col].opening >> 1;
+    	}
 }
 
 //uses sensor on the front of the robot and returns distance to nearest wall
@@ -173,10 +217,10 @@ int getDistRight(int direction){
 }
 
 int main(){
-init();
-discover(0, 3,4);
+	init();
+	discover(-1, startr,startc);
 
 
 
-return 0;
+	return 0;
 }
